2daaa2126d5dad5a0f9d0e00e8a0d09a0e0864c7
PhusCao <126900209+PhusCao@users.noreply.github.com>
2024-06-12 22:58:05 +0700

    Update main.js

--- static/js/main.js
+++ static/js/main.js
@@ -5,9 +5,9 @@ $(document).ready(function () {
         const text = $("#textToCheck").val().trim();
         originalText = text;
 
-        // Kiểm tra nếu văn bản đầu vào không rỗng
+        
         if (text) {
-            // Hiển thị biểu tượng loading hoặc thông báo cho người dùng
+            
             $("#checkBtn").prop("disabled", true).text("Đang kiểm tra...");
 
             $.ajax({
@@ -22,14 +22,14 @@ $(document).ready(function () {
                     $("#updatedText").text(correctedText);
                     $("#wordCount").text(`Số từ: ${wordCount}`);
 
-                    // Đặt lại trạng thái của nút
+                    
                     $("#checkBtn").prop("disabled", false).text("Kiểm tra");
                 },
                 error: (xhr, status, error) => {
                     console.error("Lỗi: ", status, error);
                     alert("Đã xảy ra lỗi. Vui lòng thử lại.");
 
-                    // Đặt lại trạng thái của nút
+                    
                     $("#checkBtn").prop("disabled", false).text("Kiểm tra");
                 }
             });
